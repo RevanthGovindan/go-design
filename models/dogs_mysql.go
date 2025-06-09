c4d6ba3a0be6ded2dfcc3e08d05f993828f7ce00
Revanth G <revanthji14@gmail.com>
2025-06-09 09:41:03 +0530

    dog breeds list

---
+++ models/dogs_mysql.go
@@ -0,0 +1,35 @@
+package models
+
+import (
+	"context"
+	"time"
+)
+
+func (d *DogBreed) AllDogBreeds() ([]*DogBreed, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	query := `select id, breed, weight_low_lbs, weight_high_lbs, 
+				cast(((weight_high_lbs+weight_low_lbs) / 2 ) as unsigned) as avg_weight,
+				lifespan, coalesce(details,''),
+				coalesce(alternate_names,''),coalesce(geographic_origin,'')
+				from dog_breeds order by breed`
+
+	var breeds []*DogBreed = make([]*DogBreed, 0)
+
+	rows, err := db.QueryContext(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var b DogBreed
+		err := rows.Scan(&b.Id, &b.Breed, &b.WeightLowLbs, &b.WeightHighLbs,
+			&b.AverageWeight, &b.Lifespan, &b.Details, &b.AlternateNames,
+			&b.GeographicOrigin)
+		if err != nil {
+			return nil, err
+		}
+		breeds = append(breeds, &b)
+	}
+	return breeds, nil
+}
